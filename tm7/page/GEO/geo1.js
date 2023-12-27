4a072afd05d63a92d6f839e5c9a32ace64bd0ce0
Tomasz Mistat <tmistat@wp.pl>
2023-12-27 20:56:49 +0100

    nowy format geo1 nie dziala czytanie Lat i Long

--- tm7/page/GEO/geo1.js
+++ tm7/page/GEO/geo1.js
@@ -1,42 +1,56 @@
 import { createWidget, widget, prop } from "@zos/ui";
-import { Distance } from "@zos/sensor";
+import { Geolocation } from "@zos/sensor";
 import { px } from "@zos/utils";
 import PageAdvanced from "../../utils/template/PageAdvanced";
-import TextByLine from "../../utils/UI/TextByLine";
+import TextByLine, { MakeYByLine } from "../../utils/UI/TextByLine";
+import {DEFAULT_COLOR,DEFAULT_COLOR_TRANSPARENT,TITLE_FONT_COLOR,TITLE_FONT_SIZE} from "../../config/constants";
 
 PageAdvanced({
   state: {
     pageName: "geo1",
   },
   build() {
-    const distance = new Distance();
+    //const distance = new Distance();
+    const geolocation = new Geolocation();
 
-    const currentText = new TextByLine({
-      text: `current:${distance.getCurrent()}`,
-      line: 0,
-    }).render();
+    const currentLat = new TextByLine({
+      //text: `current:${distance.getLatit()}`,
+      text: `Lat:${geolocation.getLatitude()}`,line: 0,}).render();
 
-    const changeEventText = new TextByLine({
-      text: `EVENT-CHANGE:${distance.getCurrent()}`,
-      line: 1,
-    }).render();
+    const currentLon = new TextByLine({
+      //text: `current:${distance.getLatit()}`,
+      text: `Lon:${geolocation.getLongitude()}`, line: 1,}).render();
+
+    const changedLat = new TextByLine({text: `EVENT-CHANGE-LAT`, line: 2,}).render();
+
+    const changedLon = new TextByLine({text: `EVENT-CHANGE-LON`, line: 3,}).render();
+
+    /*stary callback z distance
+    const changeCallback = () => {
+      changeEventText.setProperty(prop.MORE, {text: `EVENT-CHANGE: ${distance.getCurrent()}`,});
+    };
+    */
 
     const changeCallback = () => {
-      changeEventText.setProperty(prop.MORE, {
-        text: `EVENT-CHANGE: ${distance.getCurrent()}`,
-      });
-    };
+      if (geolocation.getStatus() === 'A') {
+        //console.log(geolocation.getLatitude())
+        changedLat.setProperty(prop.MORE, {text: `nLAT: ${geolocation.getLatitude()}`,});
+
+        //console.log(geolocation.getLongitude())
+        changedLon.setProperty(prop.MORE, {text: `nLON: ${geolocation.getLongitude()}`,});
+      }
+    }
 
     createWidget(widget.BUTTON, {
       x: px(80),
-      y: px(300),
-      w: px(300),
-      h: px(60),
-      radius: px(12),
-      normal_color: 0xfc6950,
-      press_color: 0xfeb4a8,
-      text: "REGISTER_CHANGE",
-      click_func: distance.onChange(changeCallback),
+      y: MakeYByLine(4),
+      w: px(240),
+      h: px(40),
+      radius: px(16),
+      normal_color: DEFAULT_COLOR,
+      press_color: DEFAULT_COLOR_TRANSPARENT,
+      text: "update",
+      click_func: geolocation.onChange(changeCallback),
     });
   },
 });
