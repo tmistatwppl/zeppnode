307f7afb7e6a37d3b93d2ac35bac51822b63d504
Tomasz Mistat <tmistat@wp.pl>
2024-01-13 19:28:00 +0100

    drobny refaktor w app.js uzyte MENU_OPTIONS do inicjalizacji modeType

--- tm8/app.js
+++ tm8/app.js
@@ -1,19 +1,20 @@
 import { log as Logger } from "@zos/utils";
 import LocalStorage from "./utils/storage";
+import { MENU_OPTIONS } from "./utils/constants";
 
 const logger = Logger.getLogger("tm8");
 const fileName = "local_data.txt";
 
 App({
   globalData: {
-    modeType: "menu2_type",
+    modeType: MENU_OPTIONS[2].type, //"menu2_type",
     localStorage: null,
   },
   onCreate() {
     logger("app onCreate invoke");
     try {
       this.globalData.localStorage = new LocalStorage(fileName);
-      const { modeType = "menu2_type" } = this.globalData.localStorage.get();
+      const { modeType } = this.globalData.localStorage.get();
       this.globalData.modeType = modeType;
     } catch (e) {
       logger.log("--->e:", e);
